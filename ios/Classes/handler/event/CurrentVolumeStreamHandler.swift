9302d13acf8db02f52d3d2cf2981c78332088e5f
Maxim Konstantinov <konstantinovmax@gmail.com>
2024-06-05 16:32:13 +0400

    fix(audible_mode): ensure events are sent on main thread in CurrentVolumeStreamHandler

--- ios/Classes/handler/event/CurrentVolumeStreamHandler.swift
+++ ios/Classes/handler/event/CurrentVolumeStreamHandler.swift
@@ -13,28 +13,31 @@ import MediaPlayer
 class CurrentVolumeStreamHandler: NSObject, FlutterStreamHandler {
     private let volumeEventChannel: FlutterEventChannel
     private var audioSession = AVAudioSession.sharedInstance()
-    
+
     init(_ eventChannel: FlutterEventChannel) {
         volumeEventChannel = eventChannel
         super.init()
         volumeEventChannel.setStreamHandler(self)
     }
-    
+
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        
         NotificationCenter.default.addObserver(
-            forName: NSNotification.Name(rawValue: Constants.VOLUME_NOTIFICATION), object: nil, queue: nil
-        ) { notification in
+            forName: NSNotification.Name(rawValue: Constants.VOLUME_NOTIFICATION),
+            object: nil,
+            queue: nil
+        ) { [weak self] notification in
+            guard let self = self else { return }
             if let volume = notification.userInfo?[Constants.VOLUME_NOTIFICATION_PARAM] as? Float {
-                events(Double(volume))
+                DispatchQueue.main.async {
+                    events(Double(volume))
+                }
             }
         }
         return nil
     }
-    
+
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         NotificationCenter.default.removeObserver(self)
         return nil
     }
 }
-
